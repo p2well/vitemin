4eaef1a2cd5e2f8fb557e7d15750abe2ba045dde
p2well <p2well@outlook.com>
2021-12-09 08:23:18 +0100

    Create initial top bar

---
+++ src/components/TopBar.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <header>
+    <nav class="container">
+      <h1>{{ msg }}</h1>
+    </nav>
+  </header>
+</template>
+
+<style scoped></style>
